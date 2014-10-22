6f6d93edc56fa4d9899ea0399fdec161edbb8ceb
chrisorwa <chrisorwa@gmail.com>
2014-10-22 14:55:29 +0300

    Create database.R

---
+++ Collection/Twitter/database.R
@@ -0,0 +1,8 @@
+# Create SQLite Database
+
+# Load Required Library
+library(RSQLite)
+
+# Create Driver and  Connection to SQLite Database
+drv <-dbDriver('SQLite')
+con = dbConnect(drv,'twitter.sqlite')
