f748989e35ef8c64ddd5c43beccecd67ef058548
chrisorwa <chrisorwa@gmail.com>
2014-10-22 14:56:37 +0300

    Delete database.R

--- Collection/database.R
+++
@@ -1,9 +0,0 @@
-
-# Create SQLite Database
-
-#Load Required Library
-library(RSQLite)
-
-# Create Driver and  Connection to SQLite Database
-drv <-dbDriver('SQLite')
-con = dbConnect(drv,'twitter.sqlite')
