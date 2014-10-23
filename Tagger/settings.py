cf0adac6da3475fee890933d111d7a1a4efdc37e
chrisorwa <chrisorwa@gmail.com>
2014-10-23 12:20:23 +0300

    Create settings.py

---
+++ Tagger/settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jun 18, 2014
+@author: chalenge
+
+General settings for the app! 
+'''
+
+#dbase Configuration settings
+
+dbaseConfig = {
+  'user': 'postgres',
+  # 'password': 'orwa',
+  # 'host': 'localhost',
+  'database': 'Umati',
+  # 'port': 5432
+}
