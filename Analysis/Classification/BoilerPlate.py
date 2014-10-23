a7af4bd995b37b00378d6962eaceb7503e374aea
chrisorwa <chrisorwa@gmail.com>
2014-10-23 10:24:36 +0300

    Create BoilerPlate.py

---
+++ Analysis/Classification/BoilerPlate.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+import pandas as pd
+import numpy as np
+from pandas.io.data import DataReader
+from datetime import datetime
+import sqlite3 
+import pandas.io.sql as psql
+# import PSQLUtils as pu
+import time
+import pickle
+
+os.getcwd()
+os.chdir('/home/phcostello/git/umati')
+# os.chdir('/home/datalab/Umati')
