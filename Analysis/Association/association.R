447e8294156be02a14913e3b14b4aa987c190fd5
chrisorwa <chrisorwa@gmail.com>
2014-11-07 11:44:10 +0300

    Update association.R

--- Analysis/Association/association.R
+++ Analysis/Association/association.R
@@ -3,54 +3,52 @@
 library(RSQLite)
 library(tm)
 
-#================== CONNECT TO DATABASE ========================================================================
+#================== CONNECT TO DATABASE ==========================================================================
 # Setup SQLite Database
 drv <-dbDriver('SQLite')
 con <-dbConnect(drv,'twitter.sqlite')
 
-#================== DEFINE KEYWORD FOR EVENT ==================================================================
+#================== DEFINE KEYWORD FOR EVENT =====================================================================
 # Keyword for Event
 event = ''
 
-#================== GET DATA FROM DATABASE ====================================================================
+#================== GET DATA FROM DATABASE =======================================================================
 # Load Data from Database
 sql <-paste("SELECT * FROM tweet_data WHERE [text] like '%",event,"%' ", sep="")
 a <-dbGetQuery(sql,con)
 
-#=================== CREATE TERM-DOCUMENT MATRIX ===============================================================
+#=================== CREATE TERM-DOCUMENT MATRIX =================================================================
 # Create Corpus of Tweets
 b <-Corpus(VectorSource(a$text))
 
 # Create a Text-Document Matrix, Remove Stopwords, Convert Text to Lowercase
 c <-TermDocumentMatrix(b,control=list(removePunctuation=TRUE,stopwords=SMART,tolower=TRUE))
 
-#=================== FIND ASSOCIATIONS FOR EVENT ================================================================
+#=================== FIND ASSOCIATIONS FOR EVENT =================================================================
 # Compute Optimal Lower Correlation Limit
-cor_limit = length(unique(a$text)/nrow(a))
+cor_limit = length(unique(a$text)/nrow(a))*0.1
 
 # Find Words Associated with Keyword
 tmp <-findAssocs(c,event,cor_limit)
 
-#==================== QUERY DB ASSOCIATED WORDS =================================================================
-
+#==================== QUERY DB FOR ASSOCIATED WORDS ==============================================================
 # Create Empy Data Frame
 df = data.frame()
 
 # Dynamic SQL Query
 for (rn in rownames(tmp))
 {
-  qry_str = paste("SELECT * FROM likoni WHERE [text] like '%", event, "%'","AND [text] like '% ", rn, "%'" ,sep="")
+  qry_str = paste("SELECT * FROM likoni WHERE [text] like '%", event, "%'","AND [text] like '% ", rn, "%' LIMIT 1000" ,sep="")
   df1 <-dbGetQuery(con,qry_str)
   df <-rbind(df,df1)
 }
 
-#================= FILTERING DUPLICATES/RETWEETS ===============================================================
-
-#Create Empty Data Frame and Matrix
+#================= FILTERING DUPLICATES/RETWEETS ==================================================================
+# Create Empty Data Frame and Matrix
 index_to_remove <-data.frame()
 mat <-matrix(nrow = nrow(b),ncol = nrow(b))
 
-#Find Retweets with String Distance Metric
+# Find Retweets with String Distance Metric
 for (i in 1:nrow(b))
 {
   for (j in 1:nrow(b))
@@ -64,4 +62,4 @@ for (i in 1:nrow(b))
 }
 
 #Remove Tweets with Duplicate Inidex
-c <-b$text[-c(index_to_remove)]
+d <-b$text[-c(index_to_remove)]
