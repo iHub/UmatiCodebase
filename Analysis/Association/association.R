15d23090e9b92d25079d822f33e751082e9160b5
chrisorwa <chrisorwa@gmail.com>
2014-11-17 09:43:10 +0300

    Update association.R

--- Analysis/Association/association.R
+++ Analysis/Association/association.R
@@ -1,73 +1,74 @@
-#=================== ASSOCIATION MINING =========================================================================
-# Load Required Libraries
-library(RSQLite)
-library(stringdist)
-library(tm)
 
-#================== CONNECT TO DATABASE ==========================================================================
-# Setup SQLite Database
-drv <-dbDriver('SQLite')
-con <-dbConnect(drv,'mpeketoni.sqlite')
+'
+Script    : Slicer
+Created   : November 21, 2014
+Author(s) : iHub Research
+Version   : v1.0
+'
 
-#================== DEFINE KEYWORD FOR EVENT =====================================================================
-# Keyword for Event
-event = 'mpeketoni'
+slicer <-function(event,number){
+    # ================= LOAD REQUIRED LIBRARIES =====================================================================
+    library(RSQLite)
+    library(stringdist)
+    library(tm)
 
-#================== GET DATA FROM DATABASE =======================================================================
-# Load Data from Database
+    #================== CONNECT TO DATABASE ===========================================================================
+    # Setup SQLite Database
+    drv <-dbDriver('SQLite')
+    con <-dbConnect(drv,'mpeketoni.sqlite')
 
-      sql <-paste("SELECT * FROM mpeketoni WHERE [text] like '%",event,"%' LIMIT 1000", sep="")
-      a <-dbGetQuery(con,sql)
-      a1 <-a[,-c(1)]
-      
-      if(dbExistsTable(con,'intermediate'))
-        {
-        dbWriteTable(con,'intermediate',a1)
-        }
+    #================== GET DATA FROM DATABASE =========================================================================
+    # Load Data from Database
+    sql <-paste("SELECT * FROM mpeketoni WHERE [text] like '%",event,"%' LIMIT ", number,"", sep="")
+    a <-dbGetQuery(con,sql)
+    a1 <-a['text']
 
-      #=================== CREATE TERM-DOCUMENT MATRIX ===========================================================
-      # Create Corpus of Tweets
-      b <-Corpus(VectorSource(a$text))
+    # Create Temporary DB table
+    dbWriteTable(con,'intermediate',a1)
   
-      # Create a Text-Document Matrix, Remove Stopwords, Convert Text to Lowercase
-      c <-TermDocumentMatrix(b,control=list(removePunctuation=TRUE,stopwords='english',tolower=TRUE))
+    #=================== CREATE TERM-DOCUMENT MATRIX =====================================================================
+    # Create Corpus of Tweets
+    b <-Corpus(VectorSource(a$text))
   
-      #=================== FIND ASSOCIATIONS FOR EVENT ===========================================================
-      # Compute Optimal Lower Correlation Limit
-      tokens = MC_tokenizer(a$text)
-      len = length(unique(tokens))
-      cor_limit = len/nrow(a)*0.1
+    # Create a Text-Document Matrix, Remove Stopwords, Convert Text to Lowercase
+    c <-TermDocumentMatrix(b,control=list(removePunctuation=TRUE,stopwords='english',tolower=TRUE))
   
-      # Find Words Associated with Keyword
-      tmp <-findAssocs(c,event,cor_limit)
+    #=================== FIND ASSOCIATIONS FOR EVENT =====================================================================
+    # Compute Optimal Lower Correlation Limit
+    tokens = MC_tokenizer(a$text)
+    len = length(unique(tokens))
+    cor_limit = len/nrow(a)*0.1
   
-      #==================== QUERY DB FOR ASSOCIATED WORDS ========================================================
-      # Create Empy Data Frame
-      df = data.frame()
+    # Find Words Associated with Keyword
+    tmp <-findAssocs(c,event,cor_limit)
   
-      # Dynamic SQL Query
-      for (rn in rownames(tmp))
-        {
-          qry_str = paste("SELECT * FROM intermediate WHERE [text] like '%", event, "%'","AND [text] like '% ", rn, "%'" ,sep="")
-          df1 <-dbGetQuery(con,qry_str)
-          df <-rbind(df,df1)
-          rm(df1)
-        }
+    #==================== QUERY DB FOR ASSOCIATED WORDS ====================================================================
+    # Create Empy Data Frame
+    df = data.frame()
   
-      #================= FILTERING DUPLICATES/RETWEETS ==========================================================
-      # Remove Native Retweets
-      rt = as.vector(unique(df$text))
+    # Dynamic SQL Query
+    for (rn in rownames(tmp))
+      {
+      qry_str = paste("SELECT * FROM intermediate WHERE [text] like '%", event, "%'","AND [text] like '% ", rn, "%'" ,sep="")
+      df1 <-dbGetQuery(con,qry_str)
+      df <-rbind(df,df1)
+      rm(df1)
+      }
   
-      # Create Empty Data Frame and Matrix
-      index_to_remove <-c()
-      mat <-matrix(nrow = length(rt),ncol = length(rt))
+    #================= FILTERING DUPLICATES/RETWEETS ========================================================================
+    # Remove Native Retweets
+    rt = as.vector(unique(df$text))
   
-      # Find Similar String Distance Metric
-      for (i in 1:length(rt))
-        {
-          for (j in 1:length(rt))
-            {
-              mat[i,j] <-stringdist(rt[i],rt[j],method='lv')
+    # Create Empty Data Frame and Matrix
+    index_to_remove <-c()
+    mat <-matrix(nrow = length(rt),ncol = length(rt))
+  
+    # Find Similar String Distance Metric
+    for (i in 1:length(rt))
+      {
+        for (j in 1:length(rt))
+          {
+            mat[i,j] <-stringdist(rt[i],rt[j],method='lv')
               if(!is.na(mat[i,j]))
                 {
                 if(mat[i,j] <= 20 && mat[i,j]!= 0)
@@ -77,11 +78,26 @@ event = 'mpeketoni'
                     rm(x)
                   }
                 }
-            }
-        } 
+          }
+      } 
   
-        #Remove Simialr Tweets 
-        d <- rt[-c(index_to_remove)]
-        write.csv(d,"tweets.csv")
-     }
+    # Remove Similar Tweets 
+    d <- rt[-c(index_to_remove)]
+
+    # Write CSV File
+    write.csv(d,"tweets.csv",row.names=FALSE)
+    
+    # Delete Temporary DB Table
+    dbRemoveTable(con,'intermediate')
+  }
+
+#--------------------------- Use Slicer Function -------------------------------------------------------------------------------
+
+slicer('mpeketoni', 1000)
+
+
+
+
+
+
 
