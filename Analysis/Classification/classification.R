f56114119a6967ec68ae9cc89007878cc4f37e08
chrisorwa <chrisorwa@gmail.com>
2014-11-20 10:26:49 +0300

    Update classification.R

--- Analysis/Classification/classification.R
+++ Analysis/Classification/classification.R
@@ -1,20 +1,56 @@
-#=================================================================== CLASSIFICATION ================================================================================
 
+'
+Script    : Classification
+Created   : November 21, 2014
+Author(s) : iHub Research
+Version   : v1.0
+'
+
+#=================================================================== CLASSIFICATION ================================================================================
 # Load Required Libraries
 library(e1071) 
-library(RSQLite)
 library(tm)
+library(tm)
+library(klaR)
+library(caret)
+library(RWeka)
+#=================================================================== SETTINGS ======================================================================================
+#Set Working Directory
+setwd('~')
 
-# Set Database Parameters
-drv <-dbDriver('SQLite')
-con <-dbConnect(drv,'twitter.sqlite')
+#Set Default Number of Threads
+options(mc.cores=1)
 
-# Train Data
+#Set Java Heap Size
+options(java.parameters = "-Xmx2g")
+
+#=================================================================== LOAD DATA ======================================================================================
+# Load Annotated CSV File
+a <-read.csv('~')
+
+# ==================================================================  FEATURE EXTRACTION ============================================================================
+#Create Bigram Tokenizer 
+BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+
+# Create a Term-Document Matrix
+corpus <-Corpus(VectorSource(a$text))
+tdm <-TermDocumentMatrix(corpus,control=list(removePunctuation=TRUE,tolower=TRUE,stopwords=TRUE))
+tdm1 <-removeSparseTerms(tdm,0.99)
+
+#Bind Bigrams to Bigrams as Additional Column
+features <-as.data.frame(as.matrix(tdm_1))
+compute <-cbind(features,a$label)
+
+#================================================================= CREATE PREDICTION MODEL =============================================================================
+# Select Target and Feature Variables
+x <-compute
+y <-as.factor(compute['label'])
+model <-train(x,y,'nb',trControl=trainControl(method='cv',number=10))
 
 
-# feature extraction
-x <-df[,c(40:1445)]
-y <-df[,c(12)]
+# Feature Extraction
+x <-a['label']
+y <-a['label']
 model <-naiveBayes(x,as.factor(y))
 
 # prediction
