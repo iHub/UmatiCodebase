09e38d225a0980dceb61706ea8e41eae8678ea74
chrisorwa <chrisorwa@gmail.com>
2014-11-20 14:59:28 +0300

    Update BoilerPlate.py

--- Analysis/Classification/BoilerPlate.py
+++ Analysis/Classification/BoilerPlate.py
@@ -7,10 +7,9 @@ from pandas.io.data import DataReader
 from datetime import datetime
 import sqlite3 
 import pandas.io.sql as psql
-# import PSQLUtils as pu
 import time
 import pickle
 
 os.getcwd()
-os.chdir('/home/phcostello/git/umati')
-# os.chdir('/home/datalab/Umati')
+os.chdir('~')
+
