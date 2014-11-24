7675818e7cd27a34c74517e6b30bed29d18b4111
Chalenge Masekera <chalengezw@gmail.com>
2014-11-24 00:01:37 -0800

    Merge CodeBase With My Current

--- Tagger/settings.py
+++ Tagger/settings.py
@@ -10,12 +10,27 @@ Version   : v1.0
 License   : Apache License, Version 2.0
 '''
 
-#dbase Configuration settings
+'''General settings for the app! Configures the mail platform and the database from which it will retrieve data.
+'''
 
-dbaseConfig = {
-  'user': 'postgres',
-  # 'password': '~',
-  # 'host': '~',
+#
+flask_config = dict(
+    DEBUG = True,
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL= False,
+    MAIL_USERNAME = 'your_email_address',
+    MAIL_PASSWORD = 'your_password',
+    SECRET_KEY = 'MPF\xfbz\xfbz\xa7\xcf\x84\x8cd\rg\xd5\x04\xee\xa4\xd6\xb9]\xf8\x0e\xf3'
+)
+
+#dbase Configuration settings
+dbase_config = {
+  'user': 'your_dbase_user',
+  'password': 'your_dbase_password',
+  'host': 'your_server_address',
   'database': 'Umati',
-  # 'port': 5432
+  'port': your_dbase_port
 }
+
