70da19d2f9dd68d5f9a3efb8f65f978e25feeb01
Sidney Ochieng <sidney.ochieng@gmail.com>
2015-01-15 16:13:56 +0300

    Update packages.R
    To account for errors I got when using it

--- Utilities/packages.R
+++ Utilities/packages.R
@@ -12,3 +12,8 @@ packs <- function(pkg){
 # Call Function
 packages <- c("rjson","twitteR", "streamR", "RSQLite","tm","stringr","klaR","RWeka","RCurl","RJSONIO","stringdist","openNLP","openNLPmodels.en","NLP","e1071")
 packs (packages)
+
+#If you get an error and the following packages can't be installed: twitteR,streamR and RCurl
+#In your shell:
+#apt-get -y build-dep libcurl4-gnutls-dev
+#apt-get -y install libcurl4-gnutls-dev
